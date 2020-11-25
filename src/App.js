ca5175bbec9e3daeee33a556e42fab479064f789
folafunmi-db <folafunmimustapha@gmail.com>
2020-11-25 16:40:56 +0100

    Added tweenmax staggered animation

--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
 import { useRef, useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { TweenMax, Power3 } from "gsap";
+import "./App.css";
 
 function App() {
-	let logoItem = useRef(null);
-	let textItem = useRef(null);
+	let app = useRef(null);
+	let circle = useRef(null);
+	let circleRed = useRef(null);
+	let circleBlue = useRef(null);
 
 	useEffect(() => {
-		console.log(logoItem);
+    TweenMax.to(app, 0, { css: { visibility: "visible" } });
+    
+		// TweenMax.from(circle, 0.8, { opacity: 0, x: 40, ease: Power3.easeOut });
+		// TweenMax.from(circleRed, 0.8, {
+		// 	opacity: 0,
+		// 	x: 40,
+		// 	ease: Power3.easeOut,
+		// 	delay: 0.2,
+		// });
+		// TweenMax.from(circleBlue, 0.8, {
+		// 	opacity: 0,
+		// 	x: 40,
+		// 	ease: Power3.easeOut,
+		// 	delay: 0.4,
+		// });
 
-		TweenMax.to(logoItem, 0.8, {
-			opacity: 1,
-			y: -20,
-			ease: Power3.easeOut,
-		});
-
-		TweenMax.to(textItem, 0.8, {
-			opacity: 1,
-			y: -20,
-			ease: Power3.easeOut,
-			delay: 0.4,
-		});
+		TweenMax.staggerFrom(
+			[circle, circleRed, circleBlue],
+			0.8,
+			{
+				opacity: 0,
+				x: 40,
+				ease: Power3.easeOut,
+			},
+			0.2
+		);
 	}, []);
 
 	return (
-		<div className="App">
+		<div ref={(el) => (app = el)} className="App">
 			<header className="App-header">
-				<img
-					ref={(el) => {
-						logoItem = el;
-					}}
-					src={logo}
-					className="App-logo"
-					alt="logo"
-				/>
-				<p
-					ref={(el) => {
-						textItem = el;
-					}}
-				>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+				<div className="circle-container">
+					<div ref={(el) => (circle = el)} className="circle"></div>
+					<div
+						ref={(el) => (circleRed = el)}
+						className="circle red"
+					></div>
+					<div
+						ref={(el) => (circleBlue = el)}
+						className="circle blue"
+					></div>
+				</div>
 			</header>
 		</div>
 	);
