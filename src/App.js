79fecf02c4a5dbe43cd77025535d38e0484fd4aa
folafunmi-db <folafunmimustapha@gmail.com>
2020-11-25 16:47:12 +0100

    Added growing animation to red circle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { TweenMax, Power3 } from "gsap";
 import "./App.css";
 
@@ -8,9 +8,17 @@ function App() {
 	let circleRed = useRef(null);
 	let circleBlue = useRef(null);
 
+	const handleExpand = () => {
+		TweenMax.to(circleRed, 0.8, {
+			width: 200,
+			height: 200,
+			ease: Power3.easeOut,
+		});
+	};
+
 	useEffect(() => {
-    TweenMax.to(app, 0, { css: { visibility: "visible" } });
-    
+		TweenMax.to(app, 0, { css: { visibility: "visible" } });
+
 		// TweenMax.from(circle, 0.8, { opacity: 0, x: 40, ease: Power3.easeOut });
 		// TweenMax.from(circleRed, 0.8, {
 		// 	opacity: 0,
@@ -43,6 +51,7 @@ function App() {
 				<div className="circle-container">
 					<div ref={(el) => (circle = el)} className="circle"></div>
 					<div
+						onClick={handleExpand}
 						ref={(el) => (circleRed = el)}
 						className="circle red"
 					></div>
